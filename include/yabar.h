3f9ba1a67b1b2eef090c79e699ad6feddde2e10c
losynix <losynix@openmailbox.org>
2017-01-12 23:47:52 +0100

    add disk usage block

--- include/yabar.h
+++ include/yabar.h
@@ -119,9 +119,9 @@ enum {
 
 
 #ifdef YA_INTERNAL_EWMH
-#define YA_INTERNAL_LEN 14
+#define YA_INTERNAL_LEN 15
 #else
-#define YA_INTERNAL_LEN 12
+#define YA_INTERNAL_LEN 13
 #endif
 enum {
 	YA_INT_DATE = 0,
@@ -136,6 +136,7 @@ enum {
 	YA_INT_NETWORK,
 	YA_INT_BATTERY,
 	YA_INT_VOLUME,
+	YA_INT_DISKSPACE,
 	YA_INT_TITLE,
 	YA_INT_WORKSPACE
 };
